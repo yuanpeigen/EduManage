f0bb117e132d9fb8dbb561fa0fbeac719f8db9c6
CSC-Zombie <165875533+CSC-Zombie@users.noreply.github.com>
2024-04-24 17:07:08 +0800

    create_tables.sql

--- README/create_tables.sql
+++ README/create_tables.sql
@@ -55,21 +55,28 @@ CREATE TABLE `employees` (
 
 -- 班级表 (classes)
 CREATE TABLE `classes` (
-  `class_id` INT AUTO_INCREMENT PRIMARY KEY,
-  `class_name` VARCHAR(255) NOT NULL UNIQUE,
-  `department_id` INT,
-  `description` TEXT,
-  FOREIGN KEY (`department_id`) REFERENCES `departments` (`department_id`)
+  `class_id` INT AUTO_INCREMENT PRIMARY KEY COMMENT '班级ID',
+  `class_name` VARCHAR(255) NOT NULL UNIQUE COMMENT '班级名称',
+  `class_no` INT NOT NULL COMMENT '班级序号',
+  `classroom` VARCHAR(255) COMMENT '教室',
+  `start_time` DATE NOT NULL COMMENT '创建时间',
+  `finish_time` DATE NOT NULL COMMENT '修改时间',
+
+  `class_master`VARCHAR(255) NOT NULL COMMENT '员工姓名',
+  
+  `description` TEXT COMMENT '备注'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 学员表 (students)
 CREATE TABLE `students` (
-  `student_id` INT AUTO_INCREMENT PRIMARY KEY,
-  `phone` VARCHAR(20),
-  `class_id` INT,
-  `name` VARCHAR(255) NOT NULL,
-  `date_of_birth` DATE NOT NULL,
-  `email` VARCHAR(255),
+  `student_id` INT AUTO_INCREMENT PRIMARY KEY UNIQUE COMMENT '学生ID',
+  `phone` VARCHAR(20) UNIQUE COMMENT '手机号码',
+  `class_id` INT COMMENT '班级ID',
+  `name` VARCHAR(255) NOT NULL COMMENT '班级ID',
+  `date_of_birth` DATE NOT NULL COMMENT '班级ID',
+  `gender` ENUM('W', 'M') NOT NULL COMMENT '性别 W女M男',
+  `top_degree` ENUM(1,2,3,4,5,6) COMMENT '最高学历 1初中2高中3大专4本科5硕士6博士'
+  
   FOREIGN KEY (`phone`) REFERENCES `users` (`phone`),
   FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
